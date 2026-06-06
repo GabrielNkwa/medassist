3a72679fa36318bc6e8e8d2de6529b8ae5300d1f
gabrielnkwa <gnkwa@outlook.com>
2026-06-06 22:26:40 +0100

    .l

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,4 +1,8 @@
-import { createRequestHandler } from "@vercel/node";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 import app from "../artifacts/api-server/dist/app.mjs";
 
-export default createRequestHandler(app);
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  // Make sure req and res are compatible with Express
+  app(req as any, res as any);
+}
+
