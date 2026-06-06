8672e7d84e712e166748d14821d4ddbfea53d475
gabrielnkwa <gnkwa@outlook.com>
2026-06-06 22:16:57 +0100

    ,

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,4 +1,4 @@
 import { createRequestHandler } from "@vercel/node";
-import app from "../artifacts/api-server/src/app";
+import app from "../artifacts/api-server/dist/app.mjs";
 
 export default createRequestHandler(app);
